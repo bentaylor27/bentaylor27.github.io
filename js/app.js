5df11179e9260b80dfdd44e4beb751d02e6fd3d7
user <user@users-MacBook-Air.local>
2020-10-15 00:34:21 -0500

    working on the event listener

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
 //console.log("git er done!")
 
+const speed = 1
+const direction = 1
+
+let myPlayer = document.getElementById(player);
+
+//defined the click event for all 3 buttons in lines 54-56
+
+myPlayer.addEventListener('click', function (run) {
+    run()
+})
